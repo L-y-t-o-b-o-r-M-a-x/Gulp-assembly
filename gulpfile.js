13a82cf4ae9ef3599f0124d4dbfb9046a5ee707d
Max Lagutin <L-u-t-o-b-o-r-M-a-x@yandex.ru>
2023-03-13 13:01:00 +0300

    My gulp assembly

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+// Основной модуль
+import gulp from "gulp";
+ 
+// Импорт путей
+import { path } from "./gulp/config/path.js";
+
+// Передаём значентя в глобальную переменную
+global.app = {
+    path: path,
+    gulp: gulp
+}
+
+// Импорт задач
+import { copy } from "./gulp/tasks/copy.js";
+import { reset } from "./gulp/tasks/reset.js";
+
+// Наблюдатель за изменениями в файлах
+function watcher () {
+    gulp.watch(path.watch.files, copy)
+}
+
+// Построение сценариев выполнения задач
+const dev = gulp.series(reset, copy, watcher);
+
+
+// Выполнение сценария по умолчанию
+gulp.task('default', dev );
