e79f20d7f4d09fbce4ffe770fe80f9cb0c285f62
Max Lagutin <L-u-t-o-b-o-r-M-a-x@yandex.ru>
2023-03-14 13:12:47 +0300

    Добовляет browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -18,6 +18,7 @@ global.app = {
 import { copy } from "./gulp/tasks/copy.js";
 import { reset } from "./gulp/tasks/reset.js";
 import { html } from "./gulp/tasks/html.js";
+import { server } from "./gulp/tasks/server.js";
 
 // Наблюдатель за изменениями в файлах
 function watcher () {
@@ -25,10 +26,11 @@ function watcher () {
     gulp.watch(path.watch.html, html);
 }
 
+// Основные задачи
 const mainTasks = gulp.parallel(copy, html);
 
 // Построение сценариев выполнения задач
-const dev = gulp.series(reset, mainTasks, watcher);
+const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
 
 
 // Выполнение сценария по умолчанию
