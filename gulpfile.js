fd060d78cae2cfd4577522594a9b63743a919cc3
Max Lagutin <L-u-t-o-b-o-r-M-a-x@yandex.ru>
2023-03-17 15:31:03 +0300

    feat: Создание зип архива

--- gulpfile.js
+++ gulpfile.js
@@ -26,6 +26,7 @@ import { js } from "./gulp/tasks/js.js";
 import { images } from "./gulp/tasks/images.js";
 import { otfToTtf, ttfToWoff, fontsStyle} from "./gulp/tasks/fonts.js";
 import { svgSprive } from "./gulp/tasks/svgSprive.js";
+import { zip } from "./gulp/tasks/zip.js";
 
 // Наблюдатель за изменениями в файлах
 function watcher () {
@@ -47,10 +48,12 @@ const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, images)
 // Построение сценариев выполнения задач
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
 const build = gulp.series(reset, mainTasks);
+const deployZIP = gulp.series(reset, mainTasks, zip);
 
 // Экспорт сценариев
 export { dev }
 export { build }
+export { deployZIP }
 
 
 // Выполнение сценария по умолчанию
